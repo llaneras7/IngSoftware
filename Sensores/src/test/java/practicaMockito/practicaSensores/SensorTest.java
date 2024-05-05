af1733f85b83319459b6de2bd01e37a81e2022ec
Angel Fernandez <angel.estepona@gmail.com>
2024-05-05 20:54:42 +0200

    [Practica-7] First

---
+++ Sensores/src/test/java/practicaMockito/practicaSensores/SensorTest.java
@@ -0,0 +1,11 @@
+package practicaMockito.practicaSensores;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class SensorTest {
+
+}
